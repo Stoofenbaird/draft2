50f48d9ccda92a34003ca9129737168b80733380
Stoofenbaird <mim@stoofenbaird.com>
2015-07-26 13:49:40 -0500

    Started work on new_listname files

--- dbpn/app/controllers/listnames_controller.rb
+++ dbpn/app/controllers/listnames_controller.rb
@@ -8,6 +8,18 @@ class ListnamesController < ApplicationController
     @listnames = Listname.new
   end
 
+  def create
+    @listname = Listname.new(listname_params)
+
+    respond_to do |format|
+      if @listname.save
+        format.html { redirect_to @listname, notice: "New name was successfully added." }
+      else
+        format.html { render :new }
+      end
+    end
+  end
+
   def show_animals
     @animals = Animal.where(:listname_id) == params[:id]
   end
